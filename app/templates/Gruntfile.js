49154c38bc3aaa7ad003dc2df46bb2480f5e1c39
Patrick Hulce <patrick.hulce@gmail.com>
2014-04-25 16:44:15 -0400

    Added server to Grunt and changed server destination

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -1,11 +1,17 @@
 module.exports = function (grunt) {
+  var staticFolder = 'www-root';
+  var destPrefix = staticFolder + '/';
+
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
     less: {
       app: {
-        files: {
-          'app/static/css/app.css': ['app/less/**/*.less']
-        }
+        files: [
+          {
+            dest: destPrefix + 'css/app.css',
+            src: ['app/less/**/*.less']
+          }
+        ]
       }
     },
     copy: {
@@ -15,42 +21,53 @@ module.exports = function (grunt) {
             expand: true,
             cwd: 'app/partials',
             src: '**',
-            dest: 'app/static/partials'
+            dest: destPrefix + 'partials'
           },
           {
             src: 'app/index.html',
-            dest: 'app/static/index.html'
+            dest: destPrefix + 'index.html'
           }
         ]
       }
     },
     concat: {
       vendor: {
-        files: {
-          'app/static/css/vendor.css': [
-            'bower_components/bootstrap/dist/css/bootstrap.css'
-          ],
-          'app/static/js/vendor.js': [
-            'bower_components/jquery/dist/jquery.js',
-            'bower_components/angular/angular.js',
-            'bower_components/bootstrap/dist/js/bootstrap.js',
-            'bower_components/angular-bootstrap/ui-bootstrap.js',
-            'bower_components/angular-route/angular-route.js',
-            'bower_components/underscore/underscore.js'
-          ]
-        }
+        files: [
+          {
+            dest: destPrefix + 'css/vendor.css',
+            src: [
+              'bower_components/bootstrap/dist/css/bootstrap.css'
+            ]},
+          {
+            dest: destPrefix + 'js/vendor.js',
+            src: [
+              'bower_components/jquery/dist/jquery.js',
+              'bower_components/angular/angular.js',
+              'bower_components/bootstrap/dist/js/bootstrap.js',
+              'bower_components/angular-bootstrap/ui-bootstrap.js',
+              'bower_components/angular-route/angular-route.js',
+              'bower_components/underscore/underscore.js'
+            ]
+          }
+        ]
       },
       app: {
-        files: {
-          'app/static/js/app.js': ['app/js/**/*.js']
-        }
+        files: [
+          {
+            dest: destPrefix + 'js/app.js',
+            src: ['app/js/**/*.js']
+          }
+        ]
       }
     },
     uglify: {
       vendor: {
-        files: {
-          'app/static/js/vendor.min.js' : 'app/static/js/vendor.js'
-        }
+        files: [
+          {
+            dest: destPrefix + 'js/vendor.min.js',
+            src: destPrefix + 'js/vendor.js'
+          }
+        ]
       }
     },
     watch: {
@@ -63,8 +80,16 @@ module.exports = function (grunt) {
         tasks: ['less']
       },
       appHtml: {
-        files: ['app/index.html','app/partials/**/*.html'],
+        files: ['app/index.html', 'app/partials/**/*.html'],
         tasks: ['copy']
+      }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9000,
+          base: staticFolder
+        }
       }
     }
   });
@@ -74,6 +99,14 @@ module.exports = function (grunt) {
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-connect');
 
-  grunt.registerTask('default', ['less', 'concat']);
+  var buildTasks = ['less', 'concat', 'copy', 'uglyify'];
+  var runTasks = ['watch', 'connect'];
+  var testTesks = [];
+  var defaultTasks = buildTasks.concat(runTasks);
+
+  grunt.registerTask('build', buildTasks);
+  grunt.registerTask('run', runTasks);
+  grunt.registerTask('default', defaultTasks);
 };
